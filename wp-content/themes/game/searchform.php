3f11147c3c05f89a37d7b3e35132bb32f532886f
bob <rpalmer@sellozo.com>
2021-05-19 22:03:41 -0500

    redo!

--- wp-content/themes/game/searchform.php
+++
@@ -1,6 +0,0 @@
-<div id="search">
-  <form method="get" id="searchform" action="<?php bloginfo('url'); ?>" >
-    <input id="s" type="text" name="s" value="<?php echo esc_html( get_search_query() ); ?>" />
-    <input id="searchsubmit" type="submit" value="<?php _e('GO', 'braygames'); ?>" />
-  </form>
-</div>
